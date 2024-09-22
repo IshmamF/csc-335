477b126d936eeb0ba286a9a35bcc6e90fe2719c9
IshmamF <ishmamf2003@gmail.com>
2024-09-21 21:17:07 -0400

    created hw3 file and completed question 1 code with two solutions

---
+++ Homework/homework_3.rkt
@@ -0,0 +1,23 @@
+(define (reverseNum x)
+  (reverseNumHelper x 0))
+
+
+(define (reverseNumHelper num curr)
+  (cond
+    ((= num 0) curr)
+    (else (reverseNumHelper (quotient num 10) (+ (modulo num 10) (* curr 10))))
+    ))
+
+(define (countPlace x)
+  (cond
+    ((< x 10) 0)
+    (else (+ (countPlace (quotient x 10)) 1))))
+
+(define (reverseNumHelper2 num place)
+  (cond
+    ((< num 10) num)
+    (else (+ (* (modulo num 10) (expt 10 place)) (reverseNumHelper2 (quotient num 10) (- place 1))))))
+
+
+(define (reverseNum2 x)
+  (reverseNumHelper2 x (countPlace x)))

a9f71aaabb4e25310d8b4b8a11e29b359ed833de
IshmamF <ishmamf2003@gmail.com>
2024-09-26 16:19:38 -0400

    implemneted accumated and abstracted it in filter and map

--- Practice/class-accumulate.rkt
+++ Practice/class-accumulate.rkt
@@ -15,3 +15,31 @@
 
 ; There's a common pattern here - abstarcted by accumulate
 
+(define accumulate
+  (lambda (op init seq)
+    (cond ((null? seq) init)
+          (else (op (car seq)(accumulate op init (cdr seq))))
+          )))
+
+(define filter
+  (lambda (pred L)
+    (accumulate (lambda (x y)
+                  (if (pred x)
+                      (cons x y)
+                      y))
+                '()
+                L)))
+
+(define map
+  (lambda (func L)
+    (accumulate (lambda (x y)
+                  (cons (func x) y))
+                '()
+                L)
+    ))
+
+; accumulate handles the 'cdr'ing down stuff 
+; Design Roles
+; x : (car L)
+; y : "accumulation of op over (cdr L)"
+
